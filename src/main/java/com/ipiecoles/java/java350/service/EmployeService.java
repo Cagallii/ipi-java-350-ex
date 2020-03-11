35308ae61959765d4f566c34a3edda41db6d091d
Jessica Michon <jessica-externe.michon@edf.fr>
2020-03-11 08:51:20 +0100

    WIP fix TRAVIS

--- src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
+++ src/main/java/com/ipiecoles/java/java350/service/EmployeService.java
@@ -72,7 +72,7 @@ public class EmployeService {
         //Création et sauvegarde en BDD de l'employé.
         Employe employe = new Employe(nom, prenom, matricule, LocalDate.now(), salaire, Entreprise.PERFORMANCE_BASE, tempsPartiel);
 
-        logger.debug("Avant sauvegarde : {}", employe.toString());
+        //logger.debug("Avant sauvegarde : {}", employe.toString());
         employeRepository.save(employe);
 
     }
